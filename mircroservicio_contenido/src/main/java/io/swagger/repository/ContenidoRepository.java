055a38edfe73554345aa537947dbd1b16fe59b2e
AdaXiang <147667984+AdaXiang@users.noreply.github.com>
2025-12-18 19:40:19 +0100

    insercicción usuarios

--- mircroservicio_contenido/src/main/java/io/swagger/repository/ContenidoRepository.java
+++
@@ -1,9 +0,0 @@
-package io.swagger.repository;
-import io.swagger.entity.ElementoEntity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-@Repository
-public interface ContenidoRepository extends JpaRepository<ElementoEntity, Integer>, JpaSpecificationExecutor<ElementoEntity> {
-}
